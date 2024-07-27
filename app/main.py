a0229b1a82be9627903e99e220f3e560b8275e04
Alejandro Casado <alejandroacasadoc@gmail.com>
2024-07-27 17:33:33 -0400

    FastApi CORS allow all

--- app/main.py
+++ app/main.py
@@ -1,8 +1,16 @@
 from fastapi import FastAPI
 from app.routes import user
 from app.routes import auth
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Permitir todas las fuentes
+    allow_credentials=True,
+    allow_methods=["*"],  # Permitir todos los métodos
+    allow_headers=["*"],  # Permitir todos los encabezados
+)
 
 app.include_router(user.router)
 app.include_router(auth.router)
