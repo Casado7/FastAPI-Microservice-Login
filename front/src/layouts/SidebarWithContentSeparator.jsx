63c5ca4ac7e9deb7477484f09e0ea9ee3133a9b1
Alejandro Casado <alejandroacasadoc@gmail.com>
2024-08-14 10:54:53 -0400

    feat: Add navigation functionality to ProfileMenu component

--- front/src/layouts/SidebarWithContentSeparator.jsx
+++ front/src/layouts/SidebarWithContentSeparator.jsx
@@ -20,8 +20,10 @@ import {
     PowerIcon,
 } from "@heroicons/react/24/solid";
 import { ChevronRightIcon, ChevronDownIcon } from "@heroicons/react/24/outline";
+import { useNavigate } from "react-router-dom";
 
 export default function SidebarWithContentSeparator() {
+    const navigate = useNavigate();
     const [open, setOpen] = React.useState(0);
 
     const handleOpen = (value) => {
@@ -51,92 +53,51 @@ export default function SidebarWithContentSeparator() {
                                 <PresentationChartBarIcon className="h-5 w-5" />
                             </ListItemPrefix>
                             <Typography color="blue-gray" className="mr-auto font-normal">
-                                Dashboard
+                                Users
                             </Typography>
                         </AccordionHeader>
                     </ListItem>
                     <AccordionBody className="py-1">
                         <List className="p-0">
-                            <ListItem>
+                            <ListItem
+                                onClick={() => {
+                                    navigate("/list_users");
+                                }}
+                            >
                                 <ListItemPrefix>
                                     <ChevronRightIcon strokeWidth={3} className="h-3 w-5" />
                                 </ListItemPrefix>
-                                Analytics
+                                Users List
                             </ListItem>
-                            <ListItem>
+                            <ListItem
+                                onClick={() => {
+                                    navigate("/add_user");
+                                }}
+                            >
                                 <ListItemPrefix>
                                     <ChevronRightIcon strokeWidth={3} className="h-3 w-5" />
                                 </ListItemPrefix>
-                                Reporting
-                            </ListItem>
-                            <ListItem>
-                                <ListItemPrefix>
-                                    <ChevronRightIcon strokeWidth={3} className="h-3 w-5" />
-                                </ListItemPrefix>
-                                Projects
-                            </ListItem>
-                        </List>
-                    </AccordionBody>
-                </Accordion>
-                <Accordion
-                    open={open === 2}
-                    icon={
-                        <ChevronDownIcon
-                            strokeWidth={2.5}
-                            className={`mx-auto h-4 w-4 transition-transform ${open === 2 ? "rotate-180" : ""}`}
-                        />
-                    }
-                >
-                    <ListItem className="p-0" selected={open === 2}>
-                        <AccordionHeader onClick={() => handleOpen(2)} className="border-b-0 p-3">
-                            <ListItemPrefix>
-                                <ShoppingBagIcon className="h-5 w-5" />
-                            </ListItemPrefix>
-                            <Typography color="blue-gray" className="mr-auto font-normal">
-                                E-Commerce
-                            </Typography>
-                        </AccordionHeader>
-                    </ListItem>
-                    <AccordionBody className="py-1">
-                        <List className="p-0">
-                            <ListItem>
-                                <ListItemPrefix>
-                                    <ChevronRightIcon strokeWidth={3} className="h-3 w-5" />
-                                </ListItemPrefix>
-                                Orders
-                            </ListItem>
-                            <ListItem>
-                                <ListItemPrefix>
-                                    <ChevronRightIcon strokeWidth={3} className="h-3 w-5" />
-                                </ListItemPrefix>
-                                Products
+                                Add User
                             </ListItem>
                         </List>
                     </AccordionBody>
                 </Accordion>
                 <hr className="my-2 border-blue-gray-50" />
-                <ListItem>
-                    <ListItemPrefix>
-                        <InboxIcon className="h-5 w-5" />
-                    </ListItemPrefix>
-                    Inbox
-                    <ListItemSuffix>
-                        <Chip value="14" size="sm" variant="ghost" color="blue-gray" className="rounded-full" />
-                    </ListItemSuffix>
-                </ListItem>
-                <ListItem>
+                <ListItem
+                    onClick={() => {
+                        navigate("/profile");
+                    }}
+                >
                     <ListItemPrefix>
                         <UserCircleIcon className="h-5 w-5" />
                     </ListItemPrefix>
                     Profile
                 </ListItem>
-                <ListItem>
-                    <ListItemPrefix>
-                        <Cog6ToothIcon className="h-5 w-5" />
-                    </ListItemPrefix>
-                    Settings
-                </ListItem>
-                <ListItem>
+                <ListItem
+                    onClick={() => {
+                        localStorage.removeItem("token");
+                        navigate("/login");
+                    }}>
                     <ListItemPrefix>
                         <PowerIcon className="h-5 w-5" />
                     </ListItemPrefix>
