b2dd4fa25dc476729cb35b2bb984cf0faaa56d0f
Alejandro Casado <alejandroacasadoc@gmail.com>
2024-08-14 10:56:01 -0400

    feat: Update sidebar title to "Auth System"

--- front/src/layouts/SidebarWithContentSeparator.jsx
+++ front/src/layouts/SidebarWithContentSeparator.jsx
@@ -34,7 +34,7 @@ export default function SidebarWithContentSeparator() {
         <Card className="h-[calc(100vh-2rem)] w-full max-w-[20rem] p-4 shadow-xl shadow-blue-gray-900/5">
             <div className="mb-2 p-4">
                 <Typography variant="h5" color="blue-gray">
-                    Sidebar
+                    Auth System
                 </Typography>
             </div>
             <List>
